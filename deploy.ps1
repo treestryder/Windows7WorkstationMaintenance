b5a0545484ad84ecf4f3be097412ef4268e85497
Nathan Hartley <nhartley@peckham.org>
2018-04-03 14:37:58 -0400

    2016-04-14T15:42:53-04:00 Adding a deployment script.

---
+++ deploy.ps1
@@ -0,0 +1,11 @@
+﻿<#
+.Synopsis
+Copies Windows 7 Maintenance and Install script to production location.
+#>
+
+$Destination = '\\nhartley\shared\SCCM\WorkstationMaintenance\Windows7'
+
+New-Item -Path $Destination -ItemType Directory -Force | Out-Null
+
+Copy-Item -Path "$PSScriptRoot\Install-Maintenance.ps1" -Destination $Destination -Verbose
+Copy-Item -Path "$PSScriptRoot\Maintenance.ps1" -Destination $Destination -Verbose
